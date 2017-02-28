a9688744d61e70fcf4db7673c13c599ab7ccc280
GyoreG <gyoreg@gmail.com>
2017-02-28 20:43:29 +0100

    Added initial version

---
+++ WpfApplication1/Item.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using WpfApplication1.Annotations;
+
+namespace WpfApplication1
+{
+    public class Item : INotifyPropertyChanged
+    {
+        private string _title;
+        private bool _isSelected;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value == _title) return;
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (value == _isSelected) return;
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public static implicit operator Item(string title)
+        {
+            return new Item { Title = title };
+        }
+
+        public override string ToString()
+        {
+            return Title + " " + IsSelected;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApplication1/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+using WpfApplication1.Annotations;
+
+namespace WpfApplication1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            MyList = new ObservableCollection<Item>();
+            MyList.CollectionChanged += MyList_CollectionChanged;
+
+            // add test data
+            MyList.Add("Item1");
+            MyList.Add("Item2");
+            MyList.Add("Item3");
+            MyList.Add("Item4");
+
+            SelectedItems = new CollectionViewSource { Source = MyList }.View;
+            SelectedItems.Filter = o => ((Item)o).IsSelected;
+        }
+
+        private void MyList_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null) foreach (var item in e.OldItems) ((Item)item).PropertyChanged -= ItemChanged;
+            if (e.NewItems != null) foreach (var item in e.NewItems) ((Item)item).PropertyChanged += ItemChanged;
+        }
+
+        private void ItemChanged(object sender, PropertyChangedEventArgs e)
+        {
+            SelectedItems.Refresh();
+        }
+
+        private bool FilterItem(object o)
+        {
+            var item = (Item)o;
+            return item.IsSelected;
+        }
+
+        public ObservableCollection<Item> MyList { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICollectionView AllItems { get; }
+        public ICollectionView SelectedItems { get; }
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
